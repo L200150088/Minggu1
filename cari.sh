f57d5dd4ca28488028e91e3584c9361a9bbb1556
khofa prayogo <khofaprayogo26@gmail.com>
2019-06-18 11:36:46 +0700

    Cari.sh

--- cari.sh
+++ cari.sh
@@ -1,14 +1,16 @@
 #!/bin/bash 
 #masukan file .sh
 read -p 'Cari Nama File (Ekstensi.sh) : ' FileName 
-if [ -e = $FileName ] 
+if [ -e $FileName ] 
 then 
-    echo File Tidak Ada
-else
     echo "Ada File pada Directori /Home/Desktop/praxis/minggu-1/hari-01" 
     find . -name "*.sh" -print
+else
+    echo "Nama File Tidak Lengkap"
+    echo "File Tidak Ditemukan" 
+    
 fi 
-if [ -s $FileName ] 
+if [ -e == $FileName ] 
 then 
     echo File Tidak Di Temukan.
 else
@@ -18,14 +20,15 @@ fi
 
 #masukan file .java
 read -p 'Cari Nama File (Ekstensi.java): ' FileName 
-if [ -e = $FileName ] 
+if [ -e $FileName ] 
 then 
-    echo File Tidak Ada
-else
     echo "Ada File pada Directori /Home/Desktop/praxis/minggu-1/hari-01" 
-    find . -name "*.java" -print 
+    find . -name "*.java" -print
+else
+    echo "Nama File Tidak Lengkap"
+    echo "File Tidak Ditemukan"  
 fi 
-if [ -s $FileName ] 
+if [ -e == $FileName ] 
 then 
     echo File Tidak Di Temukan.
 else
